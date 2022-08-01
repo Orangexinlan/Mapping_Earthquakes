34cb9b95fc8eac3adfdcc49502cff34e8f11fb3e
“Orangexinlan” <orangexinlan@gmail.com>
2022-08-01 12:03:20 -0400

    mutliple map

--- Mapping_Single_Points/static/js/config.js
+++ Mapping_Single_Points/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = pk.eyJ1Ijoib3JhbmdlbGwiLCJhIjoiY2w2NThjNTRvMG9xNTNsbXVqMG5xbTgwZSJ9.BXcKMT83dsvgzvSoi7SDCg;
+const API_KEY = "pk.eyJ1Ijoib3JhbmdlbGwiLCJhIjoiY2w2NThjNTRvMG9xNTNsbXVqMG5xbTgwZSJ9.BXcKMT83dsvgzvSoi7SDCg";
